14f1e2bf3bd93c127cbf2fa8d3149f7fc86e31b3
sfoolish <sfoolish@sfoolishs-MacBook-Pro.local>
2016-03-09 12:11:58 +0800

    add celery task queue exec test 2
    Signed-off-by: sfoolish <sfoolish@sfoolishs-MacBook-Pro.local>

--- project/flask-demo/app/main.py
+++ project/flask-demo/app/main.py
@@ -1,134 +1,63 @@
-import threading
-# import ssh
+import os
+import random
+import time
 
-from flask import Flask, jsonify, url_for, redirect, request, render_template
+import ssh
+from celery import Celery
+
+from flask import Flask, request, render_template, session, flash, redirect, \
+    url_for, jsonify
 from flask_pymongo import PyMongo
 from flask_restful import Api, Resource
 
+
 app = Flask(__name__)
+
+# MongoDB configuration
 app.config["MONGO2_HOST"] = "mongodb"
 app.config["MONGO2_PORT"] = 27017
 app.config["MONGO2_DBNAME"] = "students_db"
 # app.config["MONGO2_DBPASSWORD"] = "tsppass"
 
-global_thread = None
-
-mongo = PyMongo(app, config_prefix='MONGO2')
-APP_URL = "http://192.168.99.100:5000"
-
-# def exec_yardstick_cmd():
-#     user = "root"
-#     ip = "yardstick"
-#     key_filename = None
-#     connection = ssh.SSH(user, ip, key_filename=key_filename) 
-#     connection.wait()
-
-#     # TODO add stderr check
-#     exit_status, stdout, stderr = connection.execute(
-#         "sleep 20; yardstick -h")
-#     app.logger.info("yardstick output: %r" % stdout)
-
-# class Student(Resource):
-#     def get(self, registration=None, department=None):
-#         data = []
-#         if registration:
-#             studnet_info = mongo.db.student.find_one({"registration": registration}, {"_id": 0})
-#             if studnet_info:
-#                 return jsonify({"status": "ok", "data": studnet_info})
-#             else:
-#                 return {"response": "no student found for {}".format(registration)}
-
-#         elif department:
-#             cursor = mongo.db.student.find({"department": department}, {"_id": 0}).limit(10)
-#             for student in cursor:
-#                 student['url'] = APP_URL + url_for('students') + "/" + student.get('registration')
-#                 data.append(student)
-
-#             return jsonify({"department": department, "response": data})
-
-#         else:
-#             cursor = mongo.db.student.find({}, {"_id": 0, "update_time": 0}).limit(10)
-
-#             for student in cursor:
-#                 print student
-#                 student['url'] = APP_URL + url_for('students') + "/" + student.get('registration')
-#                 data.append(student)
-
-#             return jsonify({"response": data})
-
-#     def post(self):
-#         global global_thread
-#         global_thread = threading.Thread(target=exec_yardstick_cmd, name='yardstick')
-#         global_thread.start()
-#         data = request.get_json()
-#         if not data:
-#             data = {"response": "ERROR"}
-#             return jsonify(data)
-#         else:
-#             registration = data.get('registration')
-#             if registration:
-#                 if mongo.db.student.find_one({"registration": registration}):
-#                     return {"response": "student already exists."}
-#                 else:
-#                     mongo.db.student.insert(data)
-#             else:
-#                 return {"response": "registration number missing"}
-
-#         return redirect(url_for("students"))
-
-#     def put(self, registration):
-#         global global_thread
-#         if global_thread != None:
-#             # join with timeout 0 to check if thread task is finished
-#             global_thread.join(0)
-#             if global_thread.isAlive() == True:
-#                 app.logger.info("thread is still running...")
-#             else:
-#                 global_thread = None
-#         data = request.get_json()
-#         mongo.db.student.update({'registration': registration}, {'$set': data})
-#         return redirect(url_for("students"))
-
-#     def delete(self, registration):
-#         mongo.db.student.remove({'registration': registration})
-#         return redirect(url_for("students"))
-
-
-# class Index(Resource):
-#     def get(self):
-#         return redirect(url_for("students"))
-
-# class Hello(Resource):
-#     def get(self):
-#         return render_template("hello.html", name="abc")
-
-import os
-import random
-import time
-from flask import Flask, request, render_template, session, flash, redirect, \
-    url_for, jsonify
-from celery import Celery
-
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'top-secret!'
-
 # Celery configuration
+app.config['SECRET_KEY'] = 'top-secret!'
 app.config['CELERY_BROKER_URL'] = 'redis://redis:6379/0'
 app.config['CELERY_RESULT_BACKEND'] = 'redis://redis:6379/0'
+
+# Initialize Mongo
+mongo = PyMongo(app, config_prefix='MONGO2')
 
 # Initialize Celery
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
+APP_URL = "http://192.168.99.100:5000"
+
+############################## Remote task start ##############################
+
+def exec_yardstick_cmd():
+    user = "root"
+    ip = "yardstick"
+    key_filename = None
+    connection = ssh.SSH(user, ip, key_filename=key_filename) 
+    connection.wait()
+
+    # TODO add stderr check
+    exit_status, stdout, stderr = connection.execute(
+        "yardstick -h")
+    app.logger.info("yardstick output: %r" % stdout)
+
 
 @celery.task
-def send_async_email(msg):
-    """Background task to yardstick command."""
+def exec_async_yardstick_cmd(msg):
+    """Background task to run yardstick command."""
     with app.app_context():
-        for i in xrange(10):
-            time.sleep(1)
-            app.logger.info("yardstick command running ...")
+        app.logger.info("yardstick command running ...")
+        exec_yardstick_cmd()
+        app.logger.info("yardstick command finished.")
+        # for i in xrange(10):
+        #     time.sleep(1)
+        #     app.logger.info("yardstick command running ...")
 
 @celery.task(bind=True)
 def long_task(self):
@@ -150,6 +79,67 @@ def long_task(self):
     return {'current': 100, 'total': 100, 'status': 'Task completed!',
             'result': 42}
 
+############################### Remote task end ###############################
+
+class Student(Resource):
+    def get(self, registration=None, department=None):
+        data = []
+        if registration:
+            studnet_info = mongo.db.student.find_one({"registration": registration}, {"_id": 0})
+            if studnet_info:
+                return jsonify({"status": "ok", "data": studnet_info})
+            else:
+                return {"response": "no student found for {}".format(registration)}
+
+        elif department:
+            cursor = mongo.db.student.find({"department": department}, {"_id": 0}).limit(10)
+            for student in cursor:
+                student['url'] = APP_URL + url_for('students') + "/" + student.get('registration')
+                data.append(student)
+
+            return jsonify({"department": department, "response": data})
+
+        else:
+            cursor = mongo.db.student.find({}, {"_id": 0, "update_time": 0}).limit(10)
+
+            for student in cursor:
+                print student
+                student['url'] = APP_URL + url_for('students') + "/" + student.get('registration')
+                data.append(student)
+
+            return jsonify({"response": data})
+
+    def post(self):
+        data = request.get_json()
+        if not data:
+            data = {"response": "ERROR"}
+            return jsonify(data)
+        else:
+            registration = data.get('registration')
+            if registration:
+                if mongo.db.student.find_one({"registration": registration}):
+                    return {"response": "student already exists."}
+                else:
+                    mongo.db.student.insert(data)
+            else:
+                return {"response": "registration number missing"}
+
+        return redirect(url_for("students"))
+
+    def put(self, registration):
+        data = request.get_json()
+        mongo.db.student.update({'registration': registration}, {'$set': data})
+        return redirect(url_for("students"))
+
+    def delete(self, registration):
+        mongo.db.student.remove({'registration': registration})
+        return redirect(url_for("students"))
+
+
+class Hello(Resource):
+    def get(self):
+        return render_template("hello.html", name="abc")
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -162,11 +152,11 @@ def index():
     msg = 'This is a test email sent from a background Celery task.'
     if request.form['submit'] == 'Send':
         # send right away
-        send_async_email.delay(msg)
+        exec_async_yardstick_cmd.delay(msg)
         flash('Sending email to {0}'.format(email))
     else:
         # send in one minute
-        send_async_email.apply_async(args=[msg], countdown=60)
+        exec_async_yardstick_cmd.apply_async(args=[msg], countdown=60)
         flash('An email will be sent to {0} in one minute'.format(email))
 
     return redirect(url_for('index'))
@@ -177,7 +167,6 @@ def longtask():
     task = long_task.apply_async()
     return jsonify({}), 202, {'Location': url_for('taskstatus',
                                                   task_id=task.id)}
-
 
 @app.route('/status/<task_id>')
 def taskstatus(task_id):
@@ -209,12 +198,11 @@ def taskstatus(task_id):
     return jsonify(response)
 
 
-# api = Api(app)
-# api.add_resource(Index, "/", endpoint="index")
-# api.add_resource(Student, "/api", endpoint="students")
-# api.add_resource(Student, "/api/<string:registration>", endpoint="registration")
-# api.add_resource(Student, "/api/department/<string:department>", endpoint="department")
-# api.add_resource(Hello, "/hello", endpoint="hello")
+api = Api(app)
+api.add_resource(Student, "/api", endpoint="students")
+api.add_resource(Student, "/api/<string:registration>", endpoint="registration")
+api.add_resource(Student, "/api/department/<string:department>", endpoint="department")
+api.add_resource(Hello, "/hello", endpoint="hello")
 
 
 if __name__ == "__main__":
